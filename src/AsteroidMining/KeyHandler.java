d5ec93409a526e9a102f4c853198851db77989cd
jack <janibekjaku46@gmail.com>
2022-03-24 23:49:39 +0100

    KeyHandler working!

--- src/AsteroidMining/KeyHandler.java
+++ src/AsteroidMining/KeyHandler.java
@@ -18,37 +18,44 @@ public class KeyHandler extends KeyAdapter {
         for(GameObject obj: handler.objects) {
             if (obj.getId() == ID.Settler) {
                 switch (key) {
-                    case KeyEvent.VK_W:
+                    case KeyEvent.VK_UP:
                         obj.setVelY(-5);
                         break;
-                    case KeyEvent.VK_KP_DOWN:
+                    case KeyEvent.VK_DOWN:
                         obj.setVelY(5);
                         break;
-                    case KeyEvent.VK_KP_LEFT:
+                    case KeyEvent.VK_LEFT:
                         obj.setVelX(-5);
                         break;
-                    case KeyEvent.VK_KP_RIGHT:
+                    case KeyEvent.VK_RIGHT:
                         obj.setVelX(5);
                         break;
                 }
             }
         }
     }
-
     public void keyReleased(KeyEvent e){
         int key = e.getKeyCode();
-        if(settler!=null) {
-            switch (key) {
-                case KeyEvent.VK_W:
-                    settler.setVelY(0); break;
-                case KeyEvent.VK_KP_DOWN:
-                    settler.setVelY(0); break;
-                case KeyEvent.VK_KP_LEFT:
-                    settler.setVelX(0); break;
-                case KeyEvent.VK_KP_RIGHT:
-                    settler.setVelX(0); break;
+        for(GameObject obj: handler.objects) {
+            if (obj.getId() == ID.Settler) {
+                switch (key) {
+                    case KeyEvent.VK_UP:
+                        obj.setVelY(0);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        obj.setVelY(0);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        obj.setVelX(0);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        obj.setVelX(0);
+                        break;
+                }
             }
         }
     }
+
+
 
 }
